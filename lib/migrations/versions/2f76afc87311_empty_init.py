03173ed9aacfae33d30d1b8234b77c997c713b93
Kevin Breid <kevinbreid@yahoo.com>
2024-03-14 14:29:40 +0100

    Empty Init

---
+++ lib/migrations/versions/2f76afc87311_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2f76afc87311
+Revises: 
+Create Date: 2024-03-14 14:24:37.039398
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f76afc87311'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ lib/migrations/versions/f25dc0b0cf42_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f25dc0b0cf42
+Revises: 2f76afc87311
+Create Date: 2024-03-14 14:24:45.047711
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f25dc0b0cf42'
+down_revision = '2f76afc87311'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
